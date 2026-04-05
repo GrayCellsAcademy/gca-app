4c9960a14d400c3681352970cd69d9beed8a520b
GrayCellsAcademy <stanritvin@yahoo.com>
2026-04-05 13:21:49 -0400

    Fix LiveSession imports

--- src/live/LiveSession.jsx
+++ src/live/LiveSession.jsx
@@ -3,10 +3,10 @@ import { setDoc, doc } from "firebase/firestore";
 import {
   createSession, joinSession, startQuestion, revealQuestion, endSession,
   addToScore, onSessionChange, onAnswersChange, getTeacherClasses, db,
-} from "./firebase";
-import { gradeAnswer } from "./fractionUtils";
-import { REVIEW_QUESTIONS, TOTAL_POINTS } from "./sessionQuestions";
-import ClassworkSession from "./ClassworkSession";
+} from "../core/firebase";
+import { gradeAnswer } from "../core/utils/fractionUtils";
+import { REVIEW_QUESTIONS, TOTAL_POINTS } from "./sessionQuestions/lesson14";
+import ClassworkSession, { ClassworkTeacherView, ClassworkStudentView } from "../ClassworkSession";
 
 // Wrapper so classwork session gets the same full-screen treatment
 function ClassworkSessionWrapper({ user, onHome }) {
