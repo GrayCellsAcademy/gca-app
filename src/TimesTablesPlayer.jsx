5e1023278470c9c6d893a31d9b52fa0c2ac03824
GrayCellsAcademy <stanritvin@yahoo.com>
2026-05-18 20:03:17 -0400

    Fix TimesTablesPlayer: remove leading dash

--- src/TimesTablesPlayer.jsx
+++ src/TimesTablesPlayer.jsx
@@ -1,4 +1,4 @@
--import { useState, useEffect, useRef } from "react";
+﻿import { useState, useEffect, useRef } from "react";
 import useActivityTracking from "./core/useActivityTracking";
 import { saveProgress as fbSaveProgress, getProgress } from "./core/firebase";
 
@@ -545,6 +545,7 @@ export default function TimesTablesPlayer({ user, topic, onHome }) {
     </div>
   );
 }
+
 
 
 
